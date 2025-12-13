b7de3ef916a6fb6c8f2148b31fa2bf12c0044488
tuschimov <tuschimov@gmail.com>
2025-12-13 14:04:03 +0300

    add: Update README and add rounding functionality

---
+++ round.py
@@ -0,0 +1,7 @@
+# Round: En yakın tam sayıya yuvarlar.
+x = 4.44554
+print(round(x,2)) # Digit (x, sonrasında yazılan rakam) ne kadar olursa o kadar haneyi yuvarlar (5, 4.5, 4.45, 4.446 gibi.)
+
+x = 4.44554
+y = round(x,2)
+print(type(y))
