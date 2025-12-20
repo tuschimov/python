fdaf6715f27566ade8db63b80d79cdbadc11bd76
yusupf <tuschimov@gmail.com>
2025-12-20 14:04:20 +0300

    add: Math module

---
+++ math.py
@@ -0,0 +1,14 @@
+# Math modülü
+import math
+# from math import  floor, ceil, sqrt
+
+# floor(): Sayıyı kendisine en yakın "küçük" tam sayıya yuvarlar.abs
+print(math.floor(7.645)) # Output: 7
+# ceil(): Sayıyı kendisine en yakın "büyük" tam sayıya yuvarlar.
+print(math.ceil(-7.645)) # Output: 8
+# sqrt(): Karekök alma
+print(math.sqrt(64)) # Mutlaka float verir.
+# pow(): Üs alma
+print(math.pow(2,3))
+# log(): Logaritma alır. --> math.log(x,base)
+print(math.log(234))
