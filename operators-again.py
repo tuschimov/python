a883a790fb38bebb3036fe8941d903c824759add
yusupf <tuschimov@gmail.com>
2026-01-03 14:34:07 +0300

    add: Implement user authentication and scoring system

---
+++ operators-again.py
@@ -0,0 +1,15 @@
+"""
+kullaniciAdi = input("Lütfen kullanıcı adınızı giriniz:")
+sifre = input("Lütfen şifrenizi de giriniz:")
+if kullaniciAdi == "admin" and sifre == "1234":
+    print("Giriş başarılı!")
+else:
+    print("Terörist... Etkisizleştiriliyor 🔥")
+
+puan = int(input("Toplam puanını gir (hile yapma 😡😡😡)\n"))
+premiumHesap = False
+if puan >= 500 or premiumHesap:
+    print("Nutuk bedava geliyor. Kapına bak 🔥")
+else:
+    print("Parasız enik 🐶")
+"""
