6a628179d39496822aed8500a20493319695f788
yusupf <tuschimov@gmail.com>
2026-02-14 16:15:26 +0300

    add: listing people based on scores.

--- random.py
+++ random.py
@@ -4,3 +4,11 @@ print(uretim)
 isimler=["Ali", "Atilla", "Yalın", "Tuna", "Tahsin"]
 rd.shuffle(isimler)
 print(isimler)
+
+isimler = ["Ali", "Ece", "Kaan", "Mete", "Batu", "Veli" "Ayse"]
+
+numara = [910, 147, 458, 163, 1979, 505, 894]
+enKucukNum = min(numara)
+enYuksekNum = max(numara)
+indYuksek = numara.index(enYuksekNum)
+inDus = numara.index(enKucukNum)
