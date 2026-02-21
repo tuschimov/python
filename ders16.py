fa57c2199bfe896e604c34ca5de485e455b2ac8d
yusupf <tuschimov@gmail.com>
2026-02-21 14:39:18 +0300

    add: Added some practice projects.

---
+++ ders16.py
@@ -0,0 +1,34 @@
+# Bir uzay oyununda roket
+for i in range(11, 0, -1):
+    print(i)
+print("Ateş")
+
+karakter=["Jett", "Mario", "Ghost", "Trevor"]
+for i in karakter:
+    print(i)
+
+# Bir oyunda sadece çift sayılara basarak ilerlenilebildiğini varsayalım. Birden yirmiye kadar olan çift sayıları ekrana yazdırlım.
+for x in range(2, 21, 2):
+    print(x)
+
+# Gizli bir mesajı (örneğin: yusuf)  dikey olarak yazdırmamız gerekmekte.
+isim="Yusuf"
+for q in isim:
+    print(q)
+
+# Sınav not ortalaması.
+sinavNotlari = [75, 80, 95, 60, 100]
+sifir = 0
+for w in sinavNotlari:
+    sifir += w
+print(sifir/len(sinavNotlari))
+
+# Geçen veya kalan notlar.
+sinavSonuclari = [45, 88, 32, 70, 95, 49]
+for i in sinavSonuclari:
+    if i >= 50:
+        print("Geçen notlar:", i)
+
+# 1'den 10'a kadar kazanılan puanlar için karesini hesaplayan bir program
+for o in range(1, 11, 1):
+    print(f"Seviye {o} - Kazanılan XP: {o**2}")
